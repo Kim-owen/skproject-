0720c331dd6e5e61417f664b213a6c0a0acc8088
Provision Pal Admin <admin@provision.pal>
2026-07-21 18:49:08 +0100

    fix: change has_role function to SECURITY DEFINER to avoid RLS policies permission errors

--- supabase/unified_schema.sql
+++ supabase/unified_schema.sql
@@ -201,7 +201,7 @@ REVOKE EXECUTE ON FUNCTION public.touch_updated_at() FROM PUBLIC, anon, authenti
 
 -- SECURITY POLICIES REVISIONS
 CREATE OR REPLACE FUNCTION public.has_role(_user_id uuid, _role app_role)
-RETURNS boolean LANGUAGE sql STABLE SECURITY INVOKER SET search_path TO 'public' AS $$
+RETURNS boolean LANGUAGE sql STABLE SECURITY DEFINER SET search_path TO 'public' AS $$
   SELECT EXISTS (SELECT 1 FROM public.user_roles WHERE user_id = _user_id AND role = _role);
 $$;
 
